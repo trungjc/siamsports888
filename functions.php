c9f515c0afd9007456f6f44633487185abe184be
Dinh Code <git@dinhcode.com>
2017-02-03 09:57:40 +0700

    Add post_views_count

--- functions.php
+++ functions.php
@@ -159,3 +159,16 @@ function get_cat_slug($cat_id) {
 	$category = &get_category($cat_id);
 	return $category->slug;
 }
+
+function setPostViews($postID) {
+    $countKey = 'post_views_count';
+    $count = get_post_meta($postID, $countKey, true);
+    if($count==''){
+        $count = 0;
+        delete_post_meta($postID, $countKey);
+        add_post_meta($postID, $countKey, '0');
+    }else{
+        $count++;
+        update_post_meta($postID, $countKey, $count);
+    }
+}
