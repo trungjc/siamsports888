26ecde4bf6506e7b715ee69403322e0dc6cf0761
Dinh Code <git@dinhcode.com>
2017-02-03 11:37:23 +0700

    Add Facebook Comment

--- single.php
+++ single.php
@@ -10,6 +10,9 @@
 </div>
 <?php } wp_reset_query(); ?>
 
+<div class="fb-comments" data-href="<?php the_permalink() ?>" data-width="100%" data-numposts="5"></div>	
+
+
 <div class="dc_share clearfix">
 	    <p>SHARE THIS PAGE</p>
     <div class="social-single">
